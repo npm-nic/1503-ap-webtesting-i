c0c8eb4e926f774b6b91bd05446bdeaff90c4965
Nic <53277092+npm-nic@users.noreply.github.com>
2020-08-20 07:26:23 -0500

    ⚔️ MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,24 @@ module.exports = {
 };
 
 function success(item) {
+  item.enhancement !== 20 ? item.enhancement++ : item.enhancement;
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.enhancement -= 5;
+  } else if (item.enhancement > 15 && item.enhancement <= 16) {
+    item.enhancement -= 10;
+  } else if (item.enhancement > 16) {
+    item.enhancement--;
+  }
+
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
